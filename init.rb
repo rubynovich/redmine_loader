a9b5f7873802d838a316cc62436477cc9c1d6713
rubynovich <roman@shipiev.me>
2013-03-20 15:41:42 +0400

    del: commented extra code in init.rb

--- init.rb
+++ init.rb
@@ -25,27 +25,27 @@ Redmine::Plugin.register :redmine_loader do
   menu :project_menu, :loader, { :controller => 'loader', :action => 'new' },
     :caption => :label_menu_loader, :after => :new_issue, :param => :project_id
 
-  if Rails::VERSION::MAJOR < 3
-    # MS Project used YYYY-MM-DDTHH:MM:SS format. There no support of time zones, so time will be in UTC
-    ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(
-      :ms_xml => lambda{ |time| time.utc.strftime("%Y-%m-%dT%H:%M:%S") }
-    )
+#  if Rails::VERSION::MAJOR < 3
+#    # MS Project used YYYY-MM-DDTHH:MM:SS format. There no support of time zones, so time will be in UTC
+#    ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(
+#      :ms_xml => lambda{ |time| time.utc.strftime("%Y-%m-%dT%H:%M:%S") }
+#    )
 
-    # MS Project used YYYY-MM-DDTHH:MM:SS format. There no support of time zones, so time will be in UTC
-    ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(
-      :ms_xml => lambda{ |time| time.utc.strftime("%Y-%m-%dT%H:%M:%S") }
-    )
-  else
-    # MS Project used YYYY-MM-DDTHH:MM:SS format. There no support of time zones, so time will be in UTC
-    Date::DATE_FORMATS.merge!(
-      :ms_xml => lambda{ |time| time.utc.strftime("%Y-%m-%dT%H:%M:%S") }
-    )
+#    # MS Project used YYYY-MM-DDTHH:MM:SS format. There no support of time zones, so time will be in UTC
+#    ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(
+#      :ms_xml => lambda{ |time| time.utc.strftime("%Y-%m-%dT%H:%M:%S") }
+#    )
+#  else
+#    # MS Project used YYYY-MM-DDTHH:MM:SS format. There no support of time zones, so time will be in UTC
+#    Date::DATE_FORMATS.merge!(
+#      :ms_xml => lambda{ |time| time.utc.strftime("%Y-%m-%dT%H:%M:%S") }
+#    )
 
-    # MS Project used YYYY-MM-DDTHH:MM:SS format. There no support of time zones, so time will be in UTC
-    Date::DATE_FORMATS.merge!(
-      :ms_xml => lambda{ |time| time.utc.strftime("%Y-%m-%dT%H:%M:%S") }
-    )
-  end
+#    # MS Project used YYYY-MM-DDTHH:MM:SS format. There no support of time zones, so time will be in UTC
+#    Date::DATE_FORMATS.merge!(
+#      :ms_xml => lambda{ |time| time.utc.strftime("%Y-%m-%dT%H:%M:%S") }
+#    )
+#  end
 end
 
 if Rails::VERSION::MAJOR < 3
