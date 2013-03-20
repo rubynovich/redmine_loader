c572d1b1418580ff0a4016ab514c9c3f0c017920
rubynovich <roman@shipiev.me>
2013-03-20 13:21:57 +0400

    add: routes for Rails 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
-ActionController::Routing::Routes.draw do |map|
-  map.connect 'redmine_loader/:action', :controller => 'loader'
+if Rails::VERSION::MAJOR >= 3
+  RedmineApp::Application.routes.draw do
+    match 'redmine_loader/:action', :controller => 'loader'
+  end
+else
+  ActionController::Routing::Routes.draw do |map|
+    map.connect 'redmine_loader/:action', :controller => 'loader'
+  end
 end
