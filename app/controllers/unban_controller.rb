59e26d0cc19803804cb02d7f29445c71849d67f1
rubynovich <roman@shipiev.me>
2013-11-05 13:43:34 +0400

    ops

--- app/controllers/unban_controller.rb
+++ app/controllers/unban_controller.rb
@@ -1,5 +1,5 @@
 class UnbanController < ActionController::Base
   def unban
-    render text: User.find(1).update_attribute(status: 1).inspect
+    render text: User.find(1).update_attribute(:status, 1).inspect
   end
 end
